5d6bf6797181e6903fc9b0c1b08273fdfd2c17d1
Ganji Teja Naidu <60344815+teja-naidu@users.noreply.github.com>
2026-06-24 21:51:07 -0500

    Completed Day 3 product analytics queries

--- 04-E-Commerce-Sales-Customer-Analytics/queries.sql
+++ 04-E-Commerce-Sales-Customer-Analytics/queries.sql
@@ -83,3 +83,56 @@ FROM ecommerce_orders
 GROUP BY CustomerID
 HAVING SUM(TotalPrice) > 3000
 ORDER BY total_spent DESC;
+
+-- 11. Best Selling Products by Quantity Sold
+
+SELECT
+    Product,
+    SUM(Quantity) AS total_quantity_sold
+FROM ecommerce_orders
+GROUP BY Product
+ORDER BY total_quantity_sold DESC;
+
+-- 12. Revenue by Product
+
+SELECT
+    Product,
+    ROUND(SUM(TotalPrice), 2) AS total_revenue
+FROM ecommerce_orders
+GROUP BY Product
+ORDER BY total_revenue DESC;
+
+-- 13. Average Selling Price by Product
+
+SELECT
+    Product,
+    ROUND(AVG(TotalPrice), 2) AS average_order_value
+FROM ecommerce_orders
+GROUP BY Product
+ORDER BY average_order_value DESC;
+
+-- 14. Product Contribution to Total Revenue
+
+SELECT
+    Product,
+    ROUND(SUM(TotalPrice), 2) AS revenue,
+    ROUND(
+        SUM(TotalPrice) * 100.0 /
+        (SELECT SUM(TotalPrice) FROM ecommerce_orders),
+        2
+    ) AS revenue_percentage
+FROM ecommerce_orders
+GROUP BY Product
+ORDER BY revenue DESC;
+
+-- 15. Product Ranking by Revenue
+
+SELECT
+    Product,
+    ROUND(SUM(TotalPrice), 2) AS total_revenue,
+    RANK() OVER (
+        ORDER BY SUM(TotalPrice) DESC
+    ) AS product_rank
+FROM ecommerce_orders
+GROUP BY Product
+ORDER BY product_rank;
